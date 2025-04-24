3df04c60a528c97bbc7855c2565becab42f2e08a
tudt <tudt@its-global.vn>
2025-04-24 18:01:47 +0700

    - [NodeJS + Docker]: JD Assistant (Error require('dotenv') not working)

--- app.js
+++ app.js
@@ -8,19 +8,24 @@ const usersRouter = require('./routes/users');
 const apiUsersRouter = require('./routes/api/users');
 const cors = require('cors');
 const app = express();
+const jdRouter = require('./routes/jd');
+require('dotenv').config();
 // Enable CORS for all routes
 app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
+// Middleware to parse POST data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Config static folder (CSS, JS, Image, ...)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/users', apiUsersRouter);
+app.use('/jd', jdRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
